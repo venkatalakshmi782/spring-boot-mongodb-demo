7162f5fda32e44c57126b9cdb668c0347278c39e
venkatalakshmi782 <venkathp11@gmail.com>
2024-10-06 12:29:00 -0500

    remove mongodb and test container dependency

--- src/main/java/com/lakshmispring/product_service/model/Product.java
+++ src/main/java/com/lakshmispring/product_service/model/Product.java
@@ -4,19 +4,19 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
+
+
 
 import java.math.BigDecimal;
 
-@Document(value="product")
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class Product {
 
-    @Id
+
     private String productId;
     private String productName;
     private String description;
--- src/main/java/com/lakshmispring/product_service/repository/ProductRepository.java
+++ src/main/java/com/lakshmispring/product_service/repository/ProductRepository.java
@@ -1,7 +1,24 @@
 package com.lakshmispring.product_service.repository;
 
 import com.lakshmispring.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ProductRepository extends MongoRepository<Product, String> {
+
+import java.util.ArrayList;
+import java.util.List;
+
+public interface ProductRepository  {
+
+    public default void save(Product product){
+      System.out.println("product saved");
+    }
+
+    public default List<Product> findAll(){
+        List<Product> productList = new ArrayList<>();
+
+        Product product = new Product();
+        product.setProductId("101");
+        product.setProductName("Iphone13");
+        productList.add(product);
+        return productList;
+    }
 }
